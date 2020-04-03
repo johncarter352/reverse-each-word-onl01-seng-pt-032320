da58e3a085fec5efa64e73084177018642327d5c
John Carter <johncarter352@gmail.com>
2020-04-03 05:57:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,11 @@
 def reverse_each_word(array)
   new_array = array.split(" ")
-  reversed_array = new_array.each {|x| x.reverse!}
+  reversed_array = new_array.each {|w| w.reverse!}
   return reversed_array.join(" ")
+end
+
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  new_array.collect {|w| w.reverse!}
+  new_array.join(" ")
 end
